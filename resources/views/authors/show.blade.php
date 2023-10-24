6f860a92a8a9f2b94b99de8e2f1472988103e0f3
abimana <abimana@yk.local>
2023-10-25 00:07:13 +0700

    display authors posts'

--- resources/views/authors/show.blade.php
+++ resources/views/authors/show.blade.php
@@ -1,9 +1,10 @@
 <x-app-layout>
-    @if ($posts->count())        
+    @if ($posts->count())   
+    <h1 class="mt-10 text-3xl font-semibold">Latest post by {{ $authorName}} </h1>     
         <ul class="mt-10 space-y-10">
             @foreach ($posts as $post )
                 <x-post-item :post="$post"/>
-            @endforeach
+             @endforeach
         </ul>
     @endif    
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -20,4 +21,14 @@ Route::get('/posts/{slug}', function ($slug) {
 });
 
 
-Route::view('/test','app');
+Route::get('/authors/{author}', function($author){
+    $posts = Sheets::collection('posts')
+    ->all()
+    ->filter(fn (Post $post) => $post->author == $author);
+
+    return view('authors.show',[
+        'posts' => $posts,
+        'authorName' => $posts->first()->author_name
+    ]);
+
+});
